9f7ce7d7f63c5aee38753ae093c59265a70caacb
kadirsener1 <119889025+kadirsener1@users.noreply.github.com>
2026-09-05 01:05:34 +0300

    Rename channels file to tv247tr.json

--- tv247tr.py
+++ tv247tr.py
@@ -5,7 +5,7 @@ import re
 from urllib.parse import urlparse
 from playwright.async_api import async_playwright
 
-CHANNELS_FILE = "channels247.json"
+CHANNELS_FILE = "tv247tr.json"
 OUTPUT_FILE = "tv247.m3u"
 LIVETV_DIR = "tv247"  # Bireysel m3u8 dosyalarının kaydedileceği klasör
 MAX_CONCURRENT_TASKS = 2
